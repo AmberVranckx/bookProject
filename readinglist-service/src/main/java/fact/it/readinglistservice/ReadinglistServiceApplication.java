ac68ceb09d3c98ae3f8ab4befe0645a4771e424f
AmberVranckx <r0938855@student.thomasmore.be>
2024-10-14 17:14:17 +0200

    initial setup

---
+++ readinglist-service/src/main/java/fact/it/readinglistservice/ReadinglistServiceApplication.java
@@ -0,0 +1,13 @@
+package fact.it.readinglistservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadinglistServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadinglistServiceApplication.class, args);
+	}
+
+}
