c9073f32e7944db978b373d0456084c42b9fd58a
AmberVranckx <r0938855@student.thomasmore.be>
2024-10-14 17:03:54 +0200

    initial setup

---
+++ readinglist-service/src/test/java/fact/it/readinglistservice/ReadinglistServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package fact.it.readinglistservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadinglistServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
