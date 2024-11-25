b2f9c17106be60136e856f837013dec65b3ac6b0
AmberVranckx <r0938855@student.thomasmore.be>
2024-11-25 14:42:28 +0100

    add google auth

---
+++ api-gateway/src/main/java/fact/it/apigateway/config/SecurityConfig.java
@@ -0,0 +1,31 @@
+package fact.it.apigateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.web.reactive.config.EnableWebFlux;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity serverHttpSecurity) {
+        serverHttpSecurity
+                .authorizeExchange(exchange ->
+                        exchange.pathMatchers(HttpMethod.GET,"/products")
+                                .permitAll()
+                                .anyExchange()
+                                .authenticated()
+                )
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(withDefaults())
+                );
+        return serverHttpSecurity.build();
+    }
+}
