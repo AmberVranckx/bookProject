e9c079ef3fe3369423b01b5cc8014af82ad051f7
Amber Vranckx <113896024+AmberVranckx@users.noreply.github.com>
2024-12-16 11:53:25 +0100

    update api-gateway login

--- api-gateway/src/main/java/fact/it/apigateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/fact/it/apigateway/config/SecurityConfig.java
@@ -4,8 +4,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -14,19 +16,31 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @EnableWebFluxSecurity
 public class SecurityConfig {
 
-    @Bean
-    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity serverHttpSecurity) {
-        serverHttpSecurity
-                .authorizeExchange(exchange ->
-                        exchange.pathMatchers(HttpMethod.GET,"/books")
-                                .permitAll()
-                                .anyExchange()
-                                .authenticated()
-                )
+//    @Bean
+//    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity serverHttpSecurity) {
+//        serverHttpSecurity
+//                .authorizeExchange(exchange ->
+//                        exchange.pathMatchers(HttpMethod.GET,"/books")
+//                                .permitAll()
+//                                .anyExchange()
+//                                .authenticated()
+//                )
+//                .oauth2ResourceServer(oauth2 -> oauth2
+//                        .jwt(withDefaults())
+//                );
+//        return serverHttpSecurity.build();
+//    }
+
+        @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        return http
+                .authorizeHttpRequests(authorizeRequests -> {
+                    authorizeRequests.requestMatchers("/").permitAll();
+                    authorizeRequests.requestMatchers("/books").permitAll();
+                    authorizeRequests.anyRequest().authenticated();
+                })
                 .oauth2Login(Customizer.withDefaults())
-                .oauth2ResourceServer(oauth2 -> oauth2
-                        .jwt(withDefaults())
-                );
-        return serverHttpSecurity.build();
+                .formLogin(Customizer.withDefaults())
+                .build();
     }
 }
