d534d2e6c25e1c3d315005c9e8710d1b71a2fbea
Amber Vranckx <113896024+AmberVranckx@users.noreply.github.com>
2024-12-16 14:34:17 +0100

    update annotations

--- front-end-service/src/main/java/fact/it/frontendservice/FrontEndServiceApplication.java
+++ front-end-service/src/main/java/fact/it/frontendservice/FrontEndServiceApplication.java
@@ -2,10 +2,8 @@ package fact.it.frontendservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
-@EnableWebSecurity
 public class FrontEndServiceApplication {
 
     public static void main(String[] args) {
